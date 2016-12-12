48cb22d7b98cb97f69fbda873c8f7c96a49901ee
ChefDK User <chef@ip-10-238-167-196.ec2.internal>
2016-12-12 15:41:53 +0000

    Updated dir structure

--- .chefdk/gem/ruby/2.1.0/gems/kitchen-docker-2.3.0/test/integration/default/serverspec/default_spec.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'spec_helper'
-
-describe file('/etc/passwd') do
-  it { should be_file }
-end
---
+++ hello.rb
@@ -0,0 +1,6 @@
+file '/hello.txt' do
+	content "Hello, World! \n"
+	mode '0777'
+	owner 'root'
+	group 'root'
+end
